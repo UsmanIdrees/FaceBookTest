27569ed84e552d5b267396dc2e08777e0cb3ebee
Usman Idrees <m.usman4057@gmail.com>
2018-12-20 17:23:31 +0500

    Repo Created

---
+++ Assets/Facebookscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class Facebookscript : MonoBehaviour {
+
+	public Text IdText;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		if (!FB.IsInitialized) {
+			FB.Init ();
+		} else 
+		{
+			FB.ActivateApp ();
+		}
+	}
+
+	public void Login()
+	{
+		FB.LogInWithReadPermissions (callback: OnLogin);
+	}
+	void OnLogin(ILoginResult loginresult)
+	{
+		if (FB.IsLoggedIn) 
+		{
+			Debug.Log ("Successfully Logged In");
+			IdText.text = loginresult.AccessToken.UserId;
+		} 
+		else if (loginresult.Cancelled) 
+		{
+			Debug.Log ("Error While Logging in " + loginresult.Error);
+		}
+	}
+}
