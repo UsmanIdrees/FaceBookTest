267940a888656a258b50a79e17453ff161697aab
Usman Idrees <m.usman4057@gmail.com>
2018-12-22 16:20:45 +0500

    UPdate

--- Assets/Facebookscript.cs
+++ Assets/Facebookscript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Facebookscript : MonoBehaviour {
 
 	public Text IdText;
+	public Text Liketext;
 
 	// Use this for initialization
 	void Awake () 
@@ -32,6 +33,20 @@ public class Facebookscript : MonoBehaviour {
 		else if (loginresult.Cancelled) 
 		{
 			Debug.Log ("Error While Logging in " + loginresult.Error);
+		}
+	}
+
+	public void Like()
+	{
+		FB.API ("/me/likes", HttpMethod.GET, callback: OnPageLike);
+	}
+	void OnPageLike(IGraphResult LikeResult)
+	{
+		if (LikeResult.Cancelled || !string.IsNullOrEmpty (LikeResult.Error)) {
+			Debug.Log ("Operation CAncelled with error " + LikeResult.Error);
+		} else 
+		{
+			Liketext.text = "Success";
 		}
 	}
 }
